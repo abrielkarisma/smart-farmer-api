bb10534a4ee58f23210774523d5399ff0ffe5483
Firlana Saddam Pramadhan <firlanasaddamp@gmail.com>
2024-08-28 17:11:42 +0700

    init project

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  const data = {
+    title: "Express",
+    message: "Welcome to Express",
+  };
+  res.json(data);
+});
+
+module.exports = router;
